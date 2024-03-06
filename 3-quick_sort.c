622c697473e054e76bbd3a90bc85083c7c2e6db7
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-03-06 04:20:13 +0200

    madeChanges

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -15,14 +15,14 @@ void swap(int *a, int *b)
 }
 
 /**
- * partition - divide into pieces
+ * lomuto_partition - divide into pieces
  * @array: my array to divide
  * @low: low one
  * @high: high element
  *
  * Return: return i + 1
  */
-int partition(int array[], int low, int high)
+int lomuto_partition(int *array, int low, int high)
 {
 	int pivot = array[high];
 	int i = low - 1;
@@ -30,17 +30,16 @@ int partition(int array[], int low, int high)
 
 	for (j = low; j < high; j++)
 	{
-		if (array[j] <= pivot)
+		if (array[j] < pivot)
 		{
 			i++;
 			swap(&array[i], &array[j]);
 		}
 	}
 	swap(&array[i + 1], &array[high]);
-	print_array(array, high);
+	print_array(array, high + 1);
 	return (i + 1);
 }
-
 /**
  * quick_sort_helper - divide into pieces
  * @array: my array
@@ -51,7 +50,7 @@ void quick_sort_helper(int array[], int low, int high)
 {
 	if (low < high)
 	{
-		int pi = partition(array, low, high);
+		int pi = lomuto_partition(array, low, high);
 
 		quick_sort_helper(array, low, pi - 1);
 		quick_sort_helper(array, pi + 1, high);
